03f62ec9002654b72b2283abfcd3d7305a2941b4
futurezenkai <zunkichi4198@gmail.com>
2025-02-18 22:10:52 +0900

    fix lint

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module GasMileageLog
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     # アプリケーションのタイムゾーンを日本時間に設定
-    config.time_zone = 'Tokyo'
+    config.time_zone = "Tokyo"
     # ActiveRecord のタイムスタンプもローカル（日本時間）にする
     config.active_record.default_timezone = :local
   end
