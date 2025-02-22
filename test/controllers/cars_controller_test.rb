163b1814f98d4d512699c39c94cc703b0c2c090c
futurezenkai <zunkichi4198@gmail.com>
2025-02-22 21:19:56 +0900

    fix test2

--- test/controllers/cars_controller_test.rb
+++ test/controllers/cars_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class CarsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)  # fixtures/users.yml に :one というユーザーがある前提
+    sign_in @user
+  end
+
   test "should get new" do
     get new_car_url
     assert_response :success
